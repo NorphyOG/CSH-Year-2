1106b2dcf753a1289a69d72114e32e4023615778
NorphyOG <91422656+NorphyOG@users.noreply.github.com>
2022-05-16 10:12:19 +0200

    Übung 4

--- Übung 4/Programm/ConsoleApp1/ConsoleApp1/Program.cs
+++ Übung 4/Programm/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+//Default Parameter Value
 
-Console.WriteLine("Hello, World!");
+static void MyMethod(string country = "Norway") 
+{
+    Console.WriteLine(country);
+}
+
+static void Main(string[] args)
+{
+    MyMethod("Sweden");
+    MyMethod("India");
+    MyMethod();
+    MyMethod("USA");
+}
+
+
+//Multiple Parameters
+
+static void MyMethod(string fname, int age) 
+{
+    Console.WriteLine(fname + " is " + age);
+}
+
+static void Main(string[] args)
+{
+    MyMethod("Liam", 5);
+    MyMethod("Jenny", 8);
+    MyMethod("Anja", 31);
+}
+
+
+//Return Values
+
+static int MyMethod(int x) 
+{
+    return 5 + x;
+}
+
+static void Main(string[] args)
+{
+    Console.WriteLine(MyMethod(3));
+}
+
+
+//Named Arguments
+
+static void MyMethod(string child1, string child2, string child3) 
+{
+    Console.WriteLine("The youngest child is: " + child3);
+}
+
+static void Main(string[] args)
+{
+    MyMethod(child3: "John", child1: "Liam", child2: "Liam");
+}
