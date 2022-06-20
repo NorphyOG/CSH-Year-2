e40140d5553d7539e45728d2a677e2737deadfe0
NorphyOG <91422656+NorphyOG@users.noreply.github.com>
2022-06-20 13:20:06 +0200

    TestApp
    Vorbereitung auf Project

---
+++ Übung 6/Programm/TestApp/TestApp/Artikel.cs
@@ -0,0 +1,44 @@
+﻿namespace TestApp;
+
+public class Artikel
+{
+    private string _name;
+
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            if (!(value == null))
+            {
+                _name = value;
+            }
+        }
+    }
+
+    private string _einheit;
+
+    public string Einheit
+    {
+        get;
+        set;
+    }
+
+    private int _menge;
+
+    public int Menge
+    {
+        get;
+        set;
+    }
+
+    public Artikel(string name, string einheit, int menge)
+    {
+        Name = name;
+        Einheit = einheit;
+        Menge = menge;
+    }
+}
---
+++ Übung 6/Programm/TestApp/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TestApp;
+
+string path = "Einkaufsliste.txt";
+
+
+StreamWriter streamWriter = new StreamWriter(path);
+streamWriter.WriteLine("Du,Hurenson!");
+streamWriter.WriteLine(",Ich Box dich");
+streamWriter.Close();
+
+StreamReader streamReader = new StreamReader(path);
+string hilfe = streamReader.ReadLine();
+hilfe.Split(",");
+
+foreach (var VARIABLE in hilfe)
+{
+    Console.WriteLine(VARIABLE);    
+}
+
