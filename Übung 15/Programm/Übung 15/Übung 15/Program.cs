fe7f4f7aea3f3b1f9a0f3a1ebbdcaac41669cc25
NorphyOG <91422656+NorphyOG@users.noreply.github.com>
2022-11-09 07:54:49 +0100

    übung 15

---
+++ Übung 15/Programm/Übung 15/Übung 15/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Übung_15;
+
+Test test = new Test();
---
+++ Übung 15/Programm/Übung 15/Übung 15/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace Übung_15;
+
+public class Test
+{
+    public static void Network1()
+    {
+        
+    }
+    
+    public static void Network2()
+    {
+        
+    }
+        
+}
